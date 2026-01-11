59deb56d0df69279d422e3235b6f82a7b73f11fd
Matt Riechman-Bennett <me@mattriechmanbennett.com>
2026-01-10 22:23:17 -0800

    Update default Link handling to use something generic and get my preferred link from a variable sent in my profile

--- powershell/modules/SecurityAdminTools.Utilities/public/New-Script.ps1
+++ powershell/modules/SecurityAdminTools.Utilities/public/New-Script.ps1
@@ -1,21 +1,31 @@
 function New-Script() {
     [CmdletBinding()]
-        param(
-            # Name for the new script
-            [Parameter(Mandatory=$false, Position=0)]
-            [String]$ScriptNameOrPath = 'New-ExampleScript',
+    param(
+        # Name for the new script
+        [Parameter(Mandatory=$false, Position=0)]
+        [String]$ScriptNameOrPath = 'New-ExampleScript',
 
-            # Synopsis
-            [Parameter(Mandatory=$false)]
-            [String]$Synopsis = "Synopsis for new script",
+        # Synopsis
+        [Parameter(Mandatory=$false)]
+        [String]$Synopsis = "Synopsis for new script",
             
-            # Argument for the manifest description
-            [Parameter(Mandatory=$false)]
-            [String]$Description = "Description for new script",
+        # Argument for the manifest description
+        [Parameter(Mandatory=$false)]
+        [String]$Description = "Description for new script",
 
-            [Parameter(Mandatory=$false)]
-            [String]$Link = "https://github.com/mriechmanbennett/security-admin-tools/"
-        )
+        [Parameter(Mandatory=$false)]
+        [String]$Link
+    )
+
+    
+    if ([string]::IsNullOrWhiteSpace($Link)) {
+        $Link = if ([string]::IsNullOrEmpty($EnvNewScriptLink)) {
+            'https://www.aclu.org/give/ways-to-give'
+        } else {
+            $env:NewScriptLink
+        }
+    }
+
 
     # Determine if we were passed a script name or full path for the new script file
     if ($ScriptNameOrPath -contains "\"){
@@ -27,7 +37,16 @@ function New-Script() {
         }
     }
     else {
-        if ( $ScriptNameOrPath.EndsWith(".ps1") )
+        if ( $ScriptNameOrPath.EndsWith(".ps1") ) {
+            # If this is a path in the current directory
+            $ScriptName = $ScriptNameOrPath.Trim('.ps1')
+            $ScriptPath = $ScriptNameOrPath
+        }
+        else {
+            # If this is the name of a script with no path specified
+            $ScriptName = $ScriptNameOrPath
+            $ScriptPath = "$ScriptNameOrPath.ps1"
+        }
     }
 
 
@@ -61,17 +80,16 @@ function New-Script() {
         [Parameter(Mandatory=`$false,Position=0)]
         [string]`$Example = `"Example`",
 
-        [Parameter(Mandatory = `$false)]
+        [Parameter(Mandatory=`$false)]
         [ValidateSet('One','Two','Three','All')]
-        [string]`$Example2 = 'All',
+        [string]`$Example2 = 'All'
     )
 
     #------------ Script start ------------#
     # Delete this comment and add the script here
 
-}
-"
+}"
 
 
-    $ScriptTemplate | Out-File -FilePath ".\$ScriptName.ps1"
+    $ScriptTemplate | Out-File -FilePath $ScriptPath
 }
