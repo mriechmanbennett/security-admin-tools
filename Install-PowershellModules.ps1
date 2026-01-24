e18ee2b578e8f7f2bfccd49f71f58164d3285e9c
Matt Riechman-Bennett <me@mattriechmanbennett.com>
2026-01-24 09:35:49 -0800

    Add installation for different versions of Powershell
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Install-PowershellModules.ps1

--- Install-PowershellModules.ps1
+++ Install-PowershellModules.ps1
@@ -1,22 +1,61 @@
 [CmdletBinding()]
-    param(
-	    [Parameter(Mandatory=$false, Position=0)]
-	    [String]$Scope = "user"
-    )
+param(
+    [Parameter(Mandatory = $false, Position = 0)]
+    [String]$Scope = "user"
+)
 
+# Get current directory, then change to script root to execute
+$StartDirectory = $PWD
 Set-Location $PSScriptRoot
 $ModulesPath = "powershell\modules"
 
-$InstallPath = if ($Scope -eq "system") { 
-    "$Env:ProgramFiles\WindowsPowerShell\Modules\"
-} else {
-    "$HOME\Documents\WindowsPowerShell\Modules\"
+# If Powershell 6+
+if ($PSVersionTable.PSEdition -eq 'Core') {
+    $InstallPath = if ($Scope -eq "system") { 
+        "$Env:ProgramFiles\PowerShell\Modules\"
+    }
+    else {
+        "$HOME\Documents\PowerShell\Modules\"
+    }
 }
 
+# If Powershell 5.1
+if ($PSVersionTable.PSEdition -eq 'Desktop') {
+    $InstallPath = if ($Scope -eq "system") { 
+        "$Env:ProgramFiles\WindowsPowerShell\Modules\"
+    }
+    else {
+        "$HOME\Documents\WindowsPowerShell\Modules\"
+    }
+}
+
+# If Powershell <5.1
+
+if ($PSVersionTable.PSVersion -lt [Version]'5.1') {
+    Write-Host "PowerShell version is less than 5.1`
+    Many of these scripts/modules will not work on your system."
+    $ContinueAnyway = Read-Host -Prompt "Type 'continue' to install anyway or anything else to exit:"
+    
+    if ($ContinueAnyway -eq 'continue') {
+        $InstallPath = if ($Scope -eq "system") { 
+        "$Env:ProgramFiles\WindowsPowerShell\Modules\"
+        }
+        else {
+            "$HOME\Documents\WindowsPowerShell\Modules\"
+        }
+    }
+    else {
+        exit
+    }
+}
+
+
+# Install modules and import them in the desired profile
 $ProfilePath = if ($Scope -eq "system") {
     $PROFILE.AllUsersAllHosts
-} else { $PROFILE.CurrentUserAllHosts }
-	
+}
+else { $PROFILE.CurrentUserAllHosts }
+    
 $ModuleFolders = Get-ChildItem $ModulesPath
 
 [string[]] $ModuleNames = @()
@@ -40,6 +79,9 @@ foreach ($NewModule in $ModuleNames) {
     
     Get-Content $ProfilePath | foreach-object {
         if ($_ -match $ImportLine) { $LineFound = $true }
-        }
+    }
     if (!$LineFound) { $ImportLine | out-file $ProfilePath -append }
 }
+
+# Send us back to the original directory the script was called from
+Set-Location $StartDirectory
