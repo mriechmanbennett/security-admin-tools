b68ca0fc89464dcdc87938b22ba15a37d4613ce8
Matt Riechman-Bennett <me@mattriechmanbennett.com>
2026-01-25 11:11:25 -0800

    Add script to set variables used by SAT2 meta scripts

---
+++ scripts-meta/files/SAT2_Vars_Config.ps1
@@ -0,0 +1,9 @@
+# 
+$SAT2_Var_New = @{
+    SAT2VarsSet = $true
+    #AuthorName = 'Jean Tarrou'
+    #CompanyName = 'security-admin-tools'
+    #NewScriptLink = 'https://www.aclu.org/give/ways-to-give/'
+}
+
+return $SAT2_Var_New
