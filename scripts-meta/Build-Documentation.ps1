0bb38495113e00ff506bfbe4f2caa1d9118e97a2
Matt Riechman-Bennett <me@mattriechmanbennett.com>
2026-01-25 16:33:55 -0800

    Re-test powershell-module-docs/README.md

--- scripts-meta/Build-Documentation.ps1
+++ scripts-meta/Build-Documentation.ps1
@@ -63,7 +63,7 @@ $NewReadme = @"
 # SAT2 Powershell Modules
 
 Documentation for project Powershell modules.
-This README and docs are managed via [script](/../../scripts-meta/Build-Documentation.ps1)
+This README and docs are managed via [script](/../../blob/main/scripts-meta/Build-Documentation.ps1)
 
 ## Index
 
