630fa231952266b449d5aefeac632dc29b651272
Matt Riechman-Bennett <me@mattriechmanbennett.com>
2026-01-28 23:49:32 -0800

    Commit draft Get-PatchStatus

--- powershell/modules/SecurityAdminTools.WindowsUpdateManagement/public/Get-PatchStatus.ps1
+++ powershell/modules/SecurityAdminTools.WindowsUpdateManagement/public/Get-PatchStatus.ps1
@@ -39,6 +39,38 @@ function Get-PatchStatus {
 
         foreach ($Cpu in $ComputerName) {
             Write-Verbose "Querying $Cpu"
+            # Get-CimInstance -ClassName Win32_QuickFixEngineering| select HotFixID, InstalledOn | sort InstalledOn -Descending
+            # wmic qfe list brief /format:table
+            $HotfixesInstalled = Get-HotFix -ComputerName localhost | Select-Object HotFixID, InstalledOn, InstalledBy, Description
+            $CaptionOutput = (Get-WmiObject Win32_OperatingSystem).Caption
+            switch -Wildcard ($CaptionOutput) {
+                '*Server 2022*' { $OSName = 'Server2022' }
+                default { Write-Verbose "No OS Name match for $Cpu"; $OSName = 'NotFound'}
+            }
+                
+            $OSVersion = (Get-Item 'HKLM:SOFTWARE\Microsoft\Windows NT\CurrentVersion').GetValue('DisplayVersion')
+
+            write-verbose "$OSName - $OSVersion"
+            $LatestKBAvailable = (Get-LatestOSBuild -OSName $OSName -OSVersion $OSVersion).'KB article'.toString()
+            $IsPatched = $LatestKBAvailable -in $HotfixesInstalled.HotFixID
+            
+            #write-verbose $HotfixesInstalled.HotFixID
+            #$MostRecentKBInstalled = Sort-Object -Descending -Property HotFixID -InputObject $HotfixesInstalled -Top 1
+            #Write-Verbose $MostRecentKBInstalled
+
+            $PatchStatus = [PSCustomObject]@{
+                Name = $Cpu
+                IsPatched = $IsPatched
+                OSName = $OSName
+                OSVersion = $OSVersion
+                HotfixesInstalled = $HotfixesInstalled
+                #KBMostRecent = $MostRecentKBInstalled.HotFixID
+                #InstallDateMostRecent = $MostRecentKBInstalled.InstalledOn
+                #InstalledByMostRecent = $MostRecentKBInstalled.InstalledBy
+                #DescriptionMostRecent = $MostRecentKBInstalled.Description
+            }
+
+            Write-Output $PatchStatus
         }
     }
 
